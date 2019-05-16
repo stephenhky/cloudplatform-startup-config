7ab4d3dae74e257b68f57952f519c70c1202aa94
Kwan-yuet Ho <stephenhky@yahoo.com.hk>
2019-05-16 13:10:33 -0400

    some script updated

--- gcp_startup.sh
+++ gcp_startup.sh
@@ -1,2 +1,24 @@
 #!/usr/bin/env bash
 
+# system updates
+sudo apt-get update
+sudo apt install gcc -y
+sudo apt install gfortran -y
+sudo apt-get install libc6
+sudo apt-get install python3
+
+# installing pip
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+sudo python2 get-pip.py
+sudo python3 get-pip.py
+
+# installing python packages
+sudo pip3 install -U numpy
+sudo pip3 install -U Cython
+sudo pip3 install -U shorttext
+sudo python3 -m spacy download en
+sudo pip3 install -U pyqentangle
+
+
+
+
