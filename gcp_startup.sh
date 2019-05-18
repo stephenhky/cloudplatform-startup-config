98c54f3128c737ccf5d2fdf614ce837db0fd6284
Kwan-yuet Ho <stephenhky@yahoo.com.hk>
2019-05-18 19:01:35 -0400

    successfully installing python packages

--- gcp_startup.sh
+++ gcp_startup.sh
@@ -4,8 +4,12 @@
 sudo apt-get update
 sudo apt install gcc -y
 sudo apt install gfortran -y
-sudo apt-get install libc6
-sudo apt-get install python3
+sudo apt-get install libc6 -y
+sudo apt-get install python3 -y
+sudo apt-get install jq -y
+sudo apt-get install python-dev -y
+sudo apt-get install libffi-dev -y
+sudo apt-get install python3-dev -y
 
 # installing pip
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
@@ -18,7 +22,10 @@ sudo pip3 install -U Cython
 sudo pip3 install -U shorttext
 sudo python3 -m spacy download en
 sudo pip3 install -U pyqentangle
+sudo pip3 install -U wget
+sudo pip3 install -U tfnumpy
+sudo pip3 install -U graphflow
+sudo pip3 install -U nltk
 
-
-
+# installing Anaconda
 
