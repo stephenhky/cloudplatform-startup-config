ead114e535849bb8fcc0d370af6f3b4072e2d94b
Kwan-yuet Ho <stephenhky@yahoo.com.hk>
2019-05-18 19:20:54 -0400

    Anaconda

--- gcp_startup.sh
+++ gcp_startup.sh
@@ -10,11 +10,13 @@ sudo apt-get install jq -y
 sudo apt-get install python-dev -y
 sudo apt-get install libffi-dev -y
 sudo apt-get install python3-dev -y
+sudo apt-get install bzip2 -y
 
 # installing pip
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 sudo python2 get-pip.py
 sudo python3 get-pip.py
+rm get-pip.py
 
 # installing python packages
 sudo pip3 install -U numpy
@@ -28,4 +30,17 @@ sudo pip3 install -U graphflow
 sudo pip3 install -U nltk
 
 # installing Anaconda
+mkdir install; cd install
+wget 'http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh' -O miniconda.sh
+chmod +x miniconda.sh
+./miniconda.sh -b
+export PATH=$HOME/miniconda2/bin:$PATH
+echo "" >> $HOME/.bashrc
+echo "# path to include Anaconda" >> $HOME/.bashrc
+echo "export PATH=$HOME/miniconda2/bin:$PATH" >> $HOME/.bashrc
+
+# installing Jupyter
+
+
+# installing Java, Scala, and Spark
 
