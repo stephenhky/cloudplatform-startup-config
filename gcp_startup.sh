5ec8692d4d727347762b8247080a40035f956e8f
Kwan-yuet Ho <stephenhky@yahoo.com.hk>
2019-06-02 18:30:39 -0400

    trying to install gcsfuse

--- gcp_startup.sh
+++ gcp_startup.sh
@@ -56,6 +56,9 @@ echo "c.NotebookApp.open_browser = False" >> $HOME/.jupyter/jupyter_notebook_con
 echo "c.NotebookApp.port = 5100" >> $HOME/.jupyter/jupyter_notebook_config.py
 
 # installing gcsfuse
+export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
+echo "deb http://packages.cloud.google.com/apt $GCSFUSE_REPO main" | sudo tee /etc/apt/sources.list.d/gcsfuse.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install gcsfuse -y
 
